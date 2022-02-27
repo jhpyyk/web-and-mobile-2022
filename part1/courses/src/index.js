43518b640f468c80ab1b6a1fadd82e3a91874a96
Juuso Pyykkönen <jhpyyk@utu.fi>
2022-02-27 17:12:10 +0200

    1.1 done

--- part1/courses/src/index.js
+++ part1/courses/src/index.js
@@ -3,27 +3,51 @@ import ReactDOM from "react-dom";
 
 const App = () => {
   const course = "Superadvanced web and mobile programming";
-  const part1 = "Basics of React";
-  const exercises1 = 8;
-  const part2 = "Using props";
-  const exercises2 = 10;
-  const part3 = "Component states";
-  const exercises3 = 12;
+  const parts = {
+    part1: "Basics of React",
+    part2: "Using props",
+    part3: "Component states",
+  };
+  const exercises = { exercise1: 8, exercise2: 10, exercise3: 12 };
 
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Total {exercises1 + exercises2 + exercises3} exercises</p>
+      <Header course={course} />
+      <Contents parts={parts} exercises={exercises} />
+      <Total exercises={exercises} />
     </div>
+  );
+};
+
+const Header = (props) => {
+  return <h1>{props.course}</h1>;
+};
+
+const Contents = (props) => {
+  return (
+    <div>
+      <p>
+        {props.parts.part1} {props.exercises.exercise1}
+      </p>
+      <p>
+        {props.parts.part2} {props.exercises.exercise2}
+      </p>
+      <p>
+        {props.parts.part3} {props.exercises.exercise3}
+      </p>
+    </div>
+  );
+};
+
+const Total = (props) => {
+  return (
+    <p>
+      Total{" "}
+      {props.exercises.exercise1 +
+        props.exercises.exercise2 +
+        props.exercises.exercise3}{" "}
+      exercises
+    </p>
   );
 };
 
