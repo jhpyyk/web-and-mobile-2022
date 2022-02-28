528b9bc0c220c5ce1dbd583751897980f92ac93a
Juuso Pyykkönen <jhpyyk@utu.fi>
2022-02-28 19:46:00 +0200

    1.7 done

--- part1/feedback/src/index.js
+++ part1/feedback/src/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
@@ -6,6 +6,31 @@ const App = () => {
   const [goodAmount, setGoodAmount] = useState(0);
   const [neutralAmount, setNeutralAmount] = useState(0);
   const [badAmount, setBadAmount] = useState(0);
+  const [average, setAverage] = useState(0);
+  const [positivePercentage, setPositivePercentage] = useState(0);
+
+  const calculateAverage = () => {
+    let total = goodAmount + neutralAmount + badAmount;
+    let weightedTotal = goodAmount - badAmount;
+
+    if (total > 0) {
+      return weightedTotal / total;
+    }
+    return 0;
+  };
+
+  const calculatePositivePercentage = () => {
+    let total = goodAmount + neutralAmount + badAmount;
+    if (total > 0) {
+      return (100 * goodAmount) / total;
+    }
+    return 0;
+  };
+
+  useEffect(() => {
+    setAverage(calculateAverage());
+    setPositivePercentage(calculatePositivePercentage());
+  }, [goodAmount, neutralAmount, badAmount]);
 
   return (
     <div className="container">
@@ -36,6 +61,8 @@ const App = () => {
       <div>{"Good: " + goodAmount}</div>
       <div>{"Neutral: " + neutralAmount}</div>
       <div>{"Bad: " + badAmount}</div>
+      <div>{"Average: " + average.toFixed(2)}</div>
+      <div>{"Positive: " + positivePercentage.toFixed(2) + "%"}</div>
     </div>
   );
 };
