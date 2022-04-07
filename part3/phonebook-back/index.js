d2d2d7654b2fb81ddda59a2130b7a694c854ced5
Juuso Pyykkönen <jhpyyk@utu.fi>
2022-04-07 16:25:10 +0300

    3.3 done

--- part3/phonebook-back/index.js
+++ part3/phonebook-back/index.js
@@ -23,6 +23,23 @@ expr.get("/api/persons", (req, res) => {
   res.json(persons);
 });
 
+expr.get("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+  const person = persons.find((person) => person.id === id);
+  if (person) {
+    response.json(person);
+  } else {
+    response.status(404).end();
+  }
+});
+
+expr.delete("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+  persons = persons.filter((person) => person.id !== id);
+
+  response.status(204).end();
+});
+
 const PORT = 3001;
 expr.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
